2c1fbe6c2b2188858ee127ecaa0358f661ea26a7
samirvithlani <samir.vithlani83955@gmail.com>
2022-11-23 19:53:40 +0530

    pattern

---
+++ loop/nestedloop/pattern2.py
@@ -0,0 +1,13 @@
+for i in range(1,6):
+    #1 #2 # 3
+    for j in range(i):
+        #j =1
+        #j =1 j =2
+        #j=1 j=2 j=3
+        print("*",end = " ")
+        #*
+        #* *
+        #* * *
+        
+        
+    print("\n")
---
+++ loop/nestedloop/pattern5.py
@@ -0,0 +1,10 @@
+k=1
+for i in range(1,6):
+    #2 
+    for j in  range(1,i+1):
+        #k =1
+        #2 3
+        print(k,end = " ")
+        k+=1 # k =2 # 3
+    
+    print("\n")    
