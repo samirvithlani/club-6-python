f14bbda0b3ee4df704f7c615c0e1384d54e14da2
samirvithlani <samir.vithlani83955@gmail.com>
2023-01-17 11:42:43 +0530

    string

---
+++ string/string1.py
@@ -0,0 +1,34 @@
+#dynamic array
+name = "hithisis pythonprogramming\n"
+name1 ="123456a"
+
+print(name)
+#name = name.upper()
+#name = name.lower()
+#name = name.title()
+#name = name.capitalize()
+#name = name.casefold()
+#name = name.center(35,"#")
+c = name.count("i",10,len(name))
+
+#flag = name.endswith("x")
+#flag = name.startswith("hi")
+index = name.find("x",6,len(name))
+#index = name.index("x")
+#flag = name.isalnum()
+#flag = name.isalpha()
+#flag = name1.isnumeric()
+#flag = name1.isdecimal()
+#flag = name.isupper()
+#flag = name.islower()
+#name = name.title()
+#flag = name.istitle()
+#flag = name.isspace()
+flag = name.isprintable()
+
+print(name)
+print(c)
+print(flag)
+print(index)
+
+
