be08d35a039933ad077bab329a9b43428b232990
samirvithlani <samir.vithlani83955@gmail.com>
2023-04-25 09:49:40 +0530

    compr

---
+++ backup/ds/list/ListDemo2.py
@@ -0,0 +1,10 @@
+#slicing in list
+users = [12,22,34,56,78,12,3,4]
+
+print(users[0:3])
+print(users[0:6:2])
+print(users[0:6:3])
+#negative indexing
+print(users[-1])
+print(users[-2])
+print(users[-1:0:-1])
---
+++ backup/ds/list/comprehensionlist/compr1.py
@@ -0,0 +1,12 @@
+users = ["raj","jay","parth","priya","amit"]
+filterdList = [i for i in users if len(i)>3 and i.startswith("p")]
+print(filterdList)
+
+# for i in users:
+#     if len(i)>3:
+#         filterdList.append(i)
+
+
+# print(filterdList)      
+
+  
