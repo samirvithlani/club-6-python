5653d88c0e475545baff112a222f27114d7125b5
samirvithlani <samir.vithlani83955@gmail.com>
2023-05-30 12:01:54 +0530

    pandas 2

---
+++ pandas/pandasSearch.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+dict = {"Name":["samir","raj","parth","sanjay"],"Age":[20,21,22,23],"City":["surat","ahmedabad","baroda","rajkot"],"salary":[10000,20000,30000,40000]}
+
+df = pd.DataFrame(dict)
+#print(df)
+#df = df[df["Age"]>22]
+#df = df[(df["Age"]>=21) & (df["salary"]>=30000)]
+# Name =start with s
+df = df[df["Name"].str.startswith("s")]
+#df = df[df["Name"]== "samir"]
+
+print(df)
